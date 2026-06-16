0f9f5d817bd52f30d1ca0ebb40d4e84697b7251b
Mani-max-code <manishanakarbodem@gmail.com>
2026-06-16 14:40:52 +0530

    Shallow&Deep Copy

--- Assignment16OpOv.cpp
+++ Assignment16OpOv.cpp
@@ -100,14 +100,15 @@ class Array{
     
     public :
       int *arr;
-      Array (){
-        arr=new int[5];
-        arr[0]=1;
-        arr[1]=2;arr[2]=3;arr[3]=4;arr[4]=5;
+      Array (Array &a){
+        arr=a.arr;
 
       }
       Array(Array &a){
-       //  arr=new int[5];
+        arr=new int[5];
+        for(int i = 0; i < 5; i++)
+        arr[i] = a.arr[i];
+
 
       }
 
